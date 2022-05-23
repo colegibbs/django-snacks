3a687b0f0e4e102de0836a903bf5b6925e1e5c31
Cole Gibbs <colegibbs0@gmail.com>
2022-05-23 14:27:21 -0700

    about tuv

--- django_snacks/snacks/urls.py
+++ django_snacks/snacks/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from .views import HomePageView
+from django.urls import URLPattern, path
+from .views import HomePageView, AboutPageView
 
-urlpattern = [
-  path('', HomePageView.as_view(), name='home')
+urlpatterns = [
+  path('', HomePageView.as_view(), name='home'),
+  path("about/", AboutPageView.as_view(), name='about')
 ]
